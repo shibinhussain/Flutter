2a0da504370d2bf749822dd2661784d6e325d3b9
Shibin Hussain <shibin.hussain@iqvia.com>
2023-10-01 16:26:32 +0530

    stateful class

---
+++ lib/dice_roller.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState() extends State<DiceRoller>{
+
+  var activeDiceImage = 'assets/images/dice-1.png';
+  void rollDice(){
+    setState(() {
+      activeDiceImage = 'assets/images/dice-2.png'
+    });
+  }
+
+  @override
+  Widget build(context){
+    return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(activeDiceImage),
+          const SizedBox(height: 20),
+          TextButton(
+            onPressed: rollDice,
+            style: TextButton.styleFrom(
+              // padding: const EdgeInsets.all(25),
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(fontSize: 20),
+            ),
+            child: const Text('SUBMIT'),
+          ),
+        ],
+      );
+  }
+}
+
+//Not working
